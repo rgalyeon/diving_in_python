0528bdea188f08f11a2a7a857736c23350fa20de
rgalyeon <dmifilin@gmail.com>
2019-06-21 13:13:45 +0300

    Add: descriptor/commission

---
+++ Week_04/ex01/commission.py
@@ -0,0 +1,22 @@
+class Value:
+    def __init__(self):
+        self.value = None
+
+    def __get__(self, instance, owner):
+        return self.value
+
+    def __set__(self, instance, value):
+        self.value = value - instance.commission * value
+
+
+class Account:
+    amount = Value()
+
+    def __init__(self, commission):
+        self.commission = commission
+
+
+if __name__ == '__main__':
+    new_account = Account(0.1)
+    new_account.amount = 100
+    print(new_account.amount)
